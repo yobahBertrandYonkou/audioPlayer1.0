2ae4c7376c53c41ad2f29a2d4fe85aa4006278ca
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2020-08-05 02:34:07 +0530

    added pause and stop functionalities

--- lib/ui/design.dart
+++ lib/ui/design.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:file_picker/file_picker.dart';
 
 bmbAudioPlayer() {
   FlutterStatusbarcolor.setStatusBarColor(Colors.black45);
-  var player = AudioPlayer();
+  var audioPlayer = AudioPlayer();
+  bool isPlaying = false;
   var body = Column(
     mainAxisAlignment: MainAxisAlignment.center,
     crossAxisAlignment: CrossAxisAlignment.center,
@@ -17,11 +19,18 @@ bmbAudioPlayer() {
           right: 20,
         ),
         width: double.infinity,
-        height: 600,
+        height: 580,
         child: Card(
+          elevation: 10,
           color: Color(0xFFFFCC00),
           child: Image.asset("images/icons.png"),
         ),
+      ),
+      Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 50,
+        child: Text("Duration and Music name"),
       ),
       Container(
         margin: EdgeInsets.only(
@@ -29,7 +38,7 @@ bmbAudioPlayer() {
           right: 20,
         ),
         width: double.infinity,
-        height: 250,
+        height: 200,
         child: Card(
           elevation: 10,
           color: Color(0xFFFFCC00),
@@ -39,24 +48,47 @@ bmbAudioPlayer() {
             children: <Widget>[
               IconButton(
                 color: Colors.white,
-                onPressed: () {},
+                onPressed: () async {
+                  String filePath = await FilePicker.getFilePath();
+                  int status = await audioPlayer.play(filePath, isLocal: true);
+                  if (status == 1) {
+                    isPlaying = true;
+                  }
+                },
                 icon: Icon(
-                  Icons.skip_previous,
+                  Icons.queue_music,
                 ),
-                iconSize: 100,
+                iconSize: 70,
                 splashColor: Colors.white,
               ),
               IconButton(
                 color: Colors.white,
-                onPressed: () async {
-                  player.play(
-                    "C:/images/test.mp3",
-                  );
-                },
+                onPressed: () {},
                 icon: Icon(
-                  Icons.play_circle_outline,
+                  Icons.skip_previous,
                 ),
-                iconSize: 200,
+                iconSize: 50,
+                splashColor: Colors.white,
+              ),
+              IconButton(
+                icon: Icon(
+                  isPlaying
+                      ? Icons.pause_circle_outline
+                      : Icons.play_circle_outline,
+                ),
+                color: Colors.white,
+                onPressed: () {
+                  print(isPlaying);
+                  if (isPlaying) {
+                    audioPlayer.pause();
+                    isPlaying = false;
+                  } else {
+                    audioPlayer.resume();
+                    isPlaying = true;
+                  }
+                  print(isPlaying);
+                },
+                iconSize: 150,
                 splashColor: Colors.white,
               ),
               IconButton(
@@ -65,7 +97,30 @@ bmbAudioPlayer() {
                 icon: Icon(
                   Icons.skip_next,
                 ),
-                iconSize: 100,
+                iconSize: 50,
+                splashColor: Colors.white,
+              ),
+              IconButton(
+                color: Colors.white,
+                onPressed: () {
+                  if (isPlaying) {
+                    audioPlayer.stop();
+                    isPlaying = false;
+                  } else {
+                    Fluttertoast.showToast(
+                        msg: "No Music",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.black38,
+                        textColor: Color(0xFFFFCC00),
+                        fontSize: 16.0);
+                  }
+                },
+                icon: Icon(
+                  Icons.stop,
+                ),
+                iconSize: 70,
                 splashColor: Colors.white,
               ),
             ],
